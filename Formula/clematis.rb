a2fb93bdede32639c09cbf14f7e371d446904c1a
vkyfka <vkyfka@gmail.com>
2025-09-28 18:30:18 +0800

    brew(formula): fix missing end; add livecheck; keep current URL/SHA/version (#106)

--- Formula/clematis.rb
+++ Formula/clematis.rb
@@ -1,25 +1,15 @@
-
-
 # typed: false
 # frozen_string_literal: true
-
 class Clematis < Formula
   include Language::Python::Virtualenv
 
   desc "Deterministic, offline-by-default multi-agent concept-graph engine"
   homepage "https://github.com/vecipher/Clematis3"
-  # CI will update url/sha256/version on tag publish
-
+  # These three are substituted below after heredoc with env vars.
   url "https://github.com/vecipher/Clematis3/releases/download/v0.8.0a4/clematis-0.8.0a4.tar.gz"
   sha256 "38e188c2137fe2d66725fee16b7fffd46322732459aab019ff4a359f564d6680"
   license "MIT"
   version "0.8.0a4"
-
-  livecheck do
-    url :stable
-    strategy :github_tags
-    regex(/^v?(\d+(?:\.\d+)+(?:[._-]?(?:a|b|rc)\d+)?)$/i)
-  
 
   depends_on "python@3.13"
 
@@ -30,5 +20,10 @@ class Clematis < Formula
   test do
     assert_match version.to_s, shell_output("#{bin}/clematis --version")
     system "#{bin}/clematis", "validate", "--json", "--help"
+  end
+
+  livecheck do
+    url :stable
+    strategy :github_latest
   end
 end
